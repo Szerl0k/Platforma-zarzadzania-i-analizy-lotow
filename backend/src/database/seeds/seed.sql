27a012e3f6399b317127cc5a7654362cd5a15094
Szerl0k <mikojoj@gmail.com>
2026-03-24 15:22:01 +0100

    seed.sql - comment out variable not available in PG16

--- backend/src/database/seeds/seed.sql
+++ backend/src/database/seeds/seed.sql
@@ -29,7 +29,8 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
+-- This line doesn't work on PG 16.
+-- SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
