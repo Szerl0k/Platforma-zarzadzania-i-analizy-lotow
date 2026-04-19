c1390377d9d07c43b1fa1b124b3d448886dad01f
Mikołaj <158087491+Szerl0k@users.noreply.github.com>
2026-04-19 19:19:53 +0200

    Add allowedDevOrigins to next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    allowedDevOrigins: ['dev-inz-frontend-app.azurewebsites.net'],
+  },
   async rewrites() {
     return [
       {
