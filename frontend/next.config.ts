2d4fa8333cc6ec1c1ab1f880d79fd1c38eb09b22
Mikołaj <158087491+Szerl0k@users.noreply.github.com>
2026-04-19 19:39:54 +0200

    Update allowed origins for server actions

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    allowedDevOrigins: ['dev-inz-frontend-app.azurewebsites.net'],
+    serverActions: {
+      allowedOrigins: ['dev-inz-frontend-app.azurewebsites.net'],
+    },
   },
   async rewrites() {
     return [
