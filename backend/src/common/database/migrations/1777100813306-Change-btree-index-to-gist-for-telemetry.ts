300e6d7b4c37c50ff8322fdfa578a657bd928020
Szerl0k <mikojoj@gmail.com>
2026-04-25 21:58:12 +0200

    apply formatting

--- backend/src/common/database/migrations/1777100813306-Change-btree-index-to-gist-for-telemetry.ts
+++ backend/src/common/database/migrations/1777100813306-Change-btree-index-to-gist-for-telemetry.ts
@@ -1,16 +1,23 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class ChangeBtreeIndexToGistForTelemetry1777100813306 implements MigrationInterface {
-    name = 'ChangeBtreeIndexToGistForTelemetry1777100813306'
+  name = "ChangeBtreeIndexToGistForTelemetry1777100813306";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "public"."IDX_19aee178ec277f23f0cf59f69a"`);
-        await queryRunner.query(`CREATE INDEX "IDX_19aee178ec277f23f0cf59f69a" ON "flight_telemetry" USING GiST ("location") `);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_19aee178ec277f23f0cf59f69a"`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_19aee178ec277f23f0cf59f69a" ON "flight_telemetry" USING GiST ("location") `,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "public"."IDX_19aee178ec277f23f0cf59f69a"`);
-        await queryRunner.query(`CREATE INDEX "IDX_19aee178ec277f23f0cf59f69a" ON "flight_telemetry" ("location") `);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_19aee178ec277f23f0cf59f69a"`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_19aee178ec277f23f0cf59f69a" ON "flight_telemetry" ("location") `,
+    );
+  }
 }
